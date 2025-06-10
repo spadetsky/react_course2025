c292fb941a78524ef790ab3d1ea32abdbceed88b
Spadetsky Mac <spadetsky@gmail.com>
2025-06-10 19:50:26 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Card = () => {
+  return (
+    <h2>Card component</h2>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+      <h2>Testing youski</h2>
+      <Card></Card>
+      <Card></Card>
+    </>
+  )
+
+}
+
+export default App
