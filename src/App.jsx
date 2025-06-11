310722be23af9f0b7e7448c4dd5a63252e70b3c5
Spadetsky Mac <spadetsky@gmail.com>
2025-06-11 21:15:55 +0200

    starting movie app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-const Card = () => {
+function App() {
   return (
-    <h2>Card component</h2>
+    <div>App</div>
   )
-}
-
-const App = () => {
-  return (
-    <>
-      <h2>Testing youski</h2>
-      <Card></Card>
-      <Card></Card>
-    </>
-  )
-
 }
 
 export default App
